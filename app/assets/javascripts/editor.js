1f10627fe415ede5158a1efcdbfd635cb9d75255
babua <onurbabacan@gmail.com>
2017-03-08 14:28:20 +0300

    reverted share functionality to old one

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -69,22 +69,22 @@ $(document).ready(function() {
 
   add_zoom_buttons();
 
-    window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '263883587393132',
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-  };
+  //   window.fbAsyncInit = function() {
+  //   FB.init({
+  //     appId      : '263883587393132',
+  //     xfbml      : true,
+  //     version    : 'v2.8'
+  //   });
+  //   FB.AppEvents.logPageView();
+  // };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  // (function(d, s, id){
+  //    var js, fjs = d.getElementsByTagName(s)[0];
+  //    if (d.getElementById(id)) {return;}
+  //    js = d.createElement(s); js.id = id;
+  //    js.src = "//connect.facebook.net/en_US/sdk.js";
+  //    fjs.parentNode.insertBefore(js, fjs);
+  //  }(document, 'script', 'facebook-jssdk'));
   
 });
 
