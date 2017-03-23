d1d43003666ea7ec48ce3289bc367fbf257b1c28
babua <onurbabacan@gmail.com>
2017-03-23 10:53:33 +0300

    added facebook OG tags

--- app/assets/javascripts/player.jsx
+++ app/assets/javascripts/player.jsx
@@ -146,9 +146,9 @@ function share_on_fb(e) {
    method: 'share',
    display: 'iframe',
    href: window.location.href,
-   hashtag: '#fretx',
-   picture: 'https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/14642496_1591739337789742_4550057457792050378_n.png?oh=104d038965bdbedc0637d86283b147ee&oe=5970F1D8',
-   quote: 'Learn how to play this song with FretX!',
+   hashtag: '#fretx'
+   // picture: 'https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/14642496_1591739337789742_4550057457792050378_n.png?oh=104d038965bdbedc0637d86283b147ee&oe=5970F1D8',
+   // quote: 'Learn how to play this song with FretX!',
  }, function(response){});
   cancelEvent(e);
 }
